2b4f0737d3762ddec6e1e57fdc526f34ea4b52d9
Harshita sahu <harshitasahu1923@gmail.com>
2026-06-22 21:12:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,18 @@ if not st.session_state.logged_in:
             st.rerun()  # This refreshes the app to show dashboard
         else:
             st.error("Invalid username or password")
-    st.info("""Demo Credentials Username: admin, Password: 123  Note: This is a demo login created for portfolio evaluation.""")
+    st.markdown("""### 💻 Amazon Laptop Analytics & Recommendation System
+    This dashboard analyzes Amazon laptop data collected through web scraping and provides:
+    - Product Performance Analysis
+    - Pricing Insights
+    - Rating Analysis
+    - Sales Trends
+    - Laptop Recommendations
+    
+    **Demo Credentials**
+    - Username: admin
+    - Password: 123
+    """)
    
 
 
